45d00b6b798e5a344846487c2ac7319923cb18e5
Rui Gonçalves <ruigoncalves@Ruis-MacBook-Pro.local>
2025-11-18 19:45:56 +0000

    ex02 CPP01 - fixed HumanB - attack()

--- CPP01/ex03/HumanB.cpp
+++ CPP01/ex03/HumanB.cpp
@@ -4,6 +4,11 @@ HumanB::HumanB(std::string name) : _name(name), _weapon(NULL) {}
 
 void HumanB::attack(void)
 {
+	if (!this->_weapon)
+	{
+		std::cout << this->_name << " has no weapon to attack!" << std::endl;
+		return ;
+	}
 	std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
 }
 
