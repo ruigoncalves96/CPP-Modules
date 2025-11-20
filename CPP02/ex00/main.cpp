694c71a8e6e3c0abf3ce6f136d0bef8ae526a2c3
Rui Gonçalves <ruigoncalves@Ruis-MacBook-Pro.local>
2025-11-20 12:46:14 +0000

    ex00 CPP02 - main improved

--- CPP02/ex00/main.cpp
+++ CPP02/ex00/main.cpp
@@ -6,11 +6,12 @@ int main(void)
 	Fixed b(a);
 	Fixed c;
 
+	a.setRawBits(42);
 	c = b;
 
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	std::cout << a.getRawBits() << " <- a" << std::endl;
+	std::cout << b.getRawBits() << " <- b" << std::endl;
+	std::cout << c.getRawBits() << " <- c" << std::endl;
 
 	return (0);
 }
