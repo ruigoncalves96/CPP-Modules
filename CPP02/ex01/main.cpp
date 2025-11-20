5adf405875bf8067a298e6d0156e9c0afa16d743
Rui Gonçalves <ruigoncalves@Ruis-MacBook-Pro.local>
2025-11-20 13:01:56 +0000

    ex01 CPP01 - main improved

--- CPP02/ex01/main.cpp
+++ CPP02/ex01/main.cpp
@@ -9,15 +9,21 @@ int main(void)
 
 	a = Fixed( 1234.4321f );
 
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
+	std::cout << "\n";
+	std::cout << "a is " << a << "\n";
+	std::cout << "b is " << b << "\n";
+	std::cout << "c is " << c << "\n";
+	std::cout << "d is " << d << "\n\n";
 
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << "\n";
+	std::cout << "b is " << b.toInt() << " as integer" << "\n";
+	std::cout << "c is " << c.toInt() << " as integer" << "\n";
+	std::cout << "d is " << d.toInt() << " as integer" << "\n\n";
+
+	std::cout << "a is " << a.toFloat() << " as float" << "\n";
+	std::cout << "b is " << b.toFloat() << " as float" << "\n";
+	std::cout << "c is " << c.toFloat() << " as float" << "\n";
+	std::cout << "d is " << d.toFloat() << " as float" << "\n\n";
 
 	return 0;	
 }
